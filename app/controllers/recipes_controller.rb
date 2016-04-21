23029ff600fec49d9ed3d5aff41960014e2e3630
Kevin Windisch <kwindisch1992@gmail.com>
2016-04-21 03:27:08 +0000

    add basic like functionality with counting and basic validation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
   end
   
   def show
@@ -36,6 +36,18 @@ class RecipesController < ApplicationController
       redirect_to recipe_path(@recipe)
     else
       render :edit
+    end
+  end
+  
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "You have voted"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only vote on a recipe once"
+      redirect_to :back
     end
   end
   
